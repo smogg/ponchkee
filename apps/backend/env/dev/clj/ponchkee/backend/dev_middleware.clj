c9b489d02517cac599c98ef68954350d35db4d1d
Oskar Zabik <oskar.zabik@gmail.com>
2025-06-29 20:31:40 +0200

    init backend

---
+++ apps/backend/env/dev/clj/ponchkee/backend/dev_middleware.clj
@@ -0,0 +1,5 @@
+(ns ponchkee.backend.dev-middleware)
+
+(defn wrap-dev [handler _opts]
+  (-> handler
+      ))
---
+++ apps/backend/test/clj/ponchkee/backend/core_test.clj
@@ -0,0 +1,8 @@
+(ns ponchkee.backend.core-test
+  (:require
+    [ponchkee.backend.test-utils :as utils]
+    [clojure.test :refer :all]))
+
+(deftest example-test
+  (is (= 1 2)))
+
